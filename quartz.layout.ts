0b58c7f5b20a21422a238358f4c97a99fbbab0b6
beni-codes <benjigalaxy69@gmail.com>
2024-09-03 19:44:03 +0300

    Quartz sync: Sep 3, 2024, 7:44 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,10 +6,7 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+
     },
   }),
 }
@@ -30,9 +27,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    
   ],
 }
 
