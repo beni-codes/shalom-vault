3edd210da13888ca18aa0b0436966867a87731b0
beni-codes <benjigalaxy69@gmail.com>
2024-09-03 20:06:41 +0300

    Quartz sync: Sep 3, 2024, 8:06 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,7 +6,10 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-
+  footer: Component.Footer({
+    links: {
+      GitHub: "https://github.com/jackyzha0/quartz",
+      "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
@@ -14,20 +17,16 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
   ],
   left: [
+    Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    
     Component.DesktopOnly(Component.TableOfContents()),
-    
   ],
 }
 
